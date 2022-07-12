f39227b9fe30742ec7217c595284613ed1d3d01b
wb-yk938100 <wb-yk938100@alibaba-inc.com>
2022-07-12 18:00:40 +0800

    update code3

--- Version/src/main/java/org/example/TestVersion.java
+++ Version/src/main/java/org/example/TestVersion.java
@@ -5,6 +5,7 @@ public class TestVersion {
     public void test1(){
         System.out.println("第一次commit");
         System.out.println("第二次commit");
+        System.out.println("第三次commit");
 
     }
 
