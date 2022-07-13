e596ff1c8ae13afd03f95cdf717cfea7b667bb16
tiddler <627617510@qq.com>
2022-07-13 08:30:44 +0800

    111

---
+++ Version/src/main/java/org/example/TestRebase.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * @title: TestRebase
+ * @Author yuke
+ * @Date: 2022/7/13 8:30
+ */
+public class TestRebase {
+
+    public void test1(){
+        System.out.println("第一次commit");
+    }
+}
