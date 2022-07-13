48e0703c24a37a00b05ebbb27cba441a1bff5c44
wb-yk938100 <wb-yk938100@alibaba-inc.com>
2022-07-13 10:43:04 +0800

    add E同时修改一条代码

---
+++ Version/src/main/java/org/example/E.java
@@ -0,0 +1,8 @@
+package org.example;
+
+/**
+ * @Author 喻可
+ * @Date 2022/7/13 10:42
+ */
+public class E {
+}
--- Version/src/main/java/org/example/TestRevert.java
+++ Version/src/main/java/org/example/TestRevert.java
@@ -9,7 +9,7 @@ public class TestRevert {
 
     public void test1(){
         System.out.println("第1次commit");
-        System.out.println("第2次commit");
+        System.out.println("第222次commit  add E同时修改一条代码");
         System.out.println("新增D 同时新增一条代码");
     }
 
